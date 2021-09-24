0b96f3bb77afbd6b686549ab0236bd868558eada
Kyle Evangelisto <kyleevangelisto@gmail.com>
2021-09-24 15:56:52 -0400

    Boilerplate js server setup
    From here on out will no longer be committing to master

--- server/app.js
+++ server/app.js
@@ -1,2 +1,16 @@
-console.log('Listening on 8888');
-app.listen(8888);
+import dotenv from "dotenv";
+import express from "express";
+import request from "request";
+import querystring from "querystring";
+
+const yelp_client_id = process.env.YELP_CLIENT_ID;
+const yelp_api_key = process.env.YELP_API_KEY;
+
+var app = express();
+var port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => console.log("Listening on port" + port));
