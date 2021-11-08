6c9e99db2fa928589d55f27ea225254eb65bce61
KhanAleXX <zhumatayev.alikhan@gmail.com>
2021-11-08 16:18:35 -0500

    changed some things

--- Messenger/key.py
+++ Messenger/key.py
@@ -1,4 +1,4 @@
 import os
 
 
-os.environ['YELP_API_KEY'] = 'HgFzZO0c1daji1sKscT7YSEVuvEYk3xPLr5d_LwFpQguqSNfV-iKq40Cu-70kDmfhS4GjF1a4gi2gi1zi7ySwAFPUAtL43wPpz6IwXF0TfgRhyZmRG-_-v2oIyhKYXYx'
+os.environ['YELP_API_KEY'] = ''
